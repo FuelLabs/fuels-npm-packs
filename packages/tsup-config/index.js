181b63233a20944fa57ecdd505f25f18462a8788
Pedro Nauck <pedronauck@gmail.com>
2023-08-08 15:01:35 -0300

    chore: improve tsup opts

--- packages/tsup-config/index.js
+++ packages/tsup-config/index.js
@@ -4,12 +4,14 @@ const path = require('path');
 
 module.exports = (_options, { withReact, ts = true } = {}) => ({
   format: ['cjs', 'esm'],
-  splitting: false,
   clean: false,
   sourcemap: true,
   shims: true,
   treeshake: true,
-  minify: process.env.NODE_ENV === 'production',
+  publicDir: 'public',
+  splitting: true,
+  metafile: true,
+  minify: process.env.NODE_ENV === 'production' ? 'terser' : false,
   ...(withReact && { inject: [path.resolve(__dirname, './react-imports.js')] }),
   ...(ts && { onSuccess: 'tsc --emitDeclarationOnly --declaration' }),
 });
