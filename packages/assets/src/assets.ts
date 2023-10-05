6895ce9a2964d400994daec11be16ba245eda527
Luiz Gomes - LuizAsFight.eth <8636507+LuizAsFight@users.noreply.github.com>
2023-10-05 19:07:24 -0300

    fix(assets): add asset configs for chainIds we know + make props optional (#27)
    make `icon` prop optional
    make `description` prop optional
    include configs for chainIds / networks

--- packages/assets/src/assets.ts
+++ packages/assets/src/assets.ts
@@ -1,22 +1,32 @@
+import { CHAIN_IDS } from './constants';
 import type { Assets } from './types';
 
 export const assets: Assets = [
   {
     name: 'Ethereum',
-    description: 'Fuel base asset',
     symbol: 'ETH',
     icon: 'eth.svg',
-    assetId:
-      '0x0000000000000000000000000000000000000000000000000000000000000000',
     networks: [
       {
         type: 'ethereum',
-        chainId: 0,
+        chainId: CHAIN_IDS.eth.sepolia,
+        decimals: 18,
+      },
+      {
+        type: 'ethereum',
+        chainId: CHAIN_IDS.eth.foundry,
         decimals: 18,
       },
       {
         type: 'fuel',
-        chainId: 0,
+        chainId: CHAIN_IDS.fuel.beta4,
+        decimals: 9,
+        assetId:
+          '0x0000000000000000000000000000000000000000000000000000000000000000',
+      },
+      {
+        type: 'fuel',
+        chainId: CHAIN_IDS.fuel.devnet,
         decimals: 9,
         assetId:
           '0x0000000000000000000000000000000000000000000000000000000000000000',
