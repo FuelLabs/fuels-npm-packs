e2929cd15e9e36a9901b5b0bfc31ede64a53467b
Hélcio Franco <helciofranco@me.com>
2024-06-03 16:07:35 -0400

    fix: prepare the useSendTransaction to receive either a wallet instance or a raw string

--- packages/react/src/hooks/useSendTransaction.ts
+++ packages/react/src/hooks/useSendTransaction.ts
@@ -1,10 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
-import type { TransactionRequestLike } from 'fuels';
+import { Address, type AbstractAddress, type TransactionRequestLike } from 'fuels';
 
 import { useFuel } from '../providers';
 
 type UseSendTransactionParams = {
-  address: string;
+  address: string | AbstractAddress;
   transaction: TransactionRequestLike;
 };
 
@@ -12,8 +12,9 @@ export const useSendTransaction = () => {
   const { fuel } = useFuel();
 
   const { mutate, mutateAsync, ...queryProps } = useMutation({
-    mutationFn: (params: UseSendTransactionParams) => {
-      return fuel.sendTransaction(params.address, params.transaction);
+    mutationFn: ({address, transaction}: UseSendTransactionParams) => {
+      const destination = Address.fromAddressOrString(address).toString();
+      return fuel.sendTransaction(destination, transaction);
     },
   });
 
