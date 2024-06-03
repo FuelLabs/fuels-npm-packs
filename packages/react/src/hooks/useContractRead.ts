1a63171d0e2b9b8b4e578935e3d5d080f04924ea
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-06-03 17:19:38 -0300

    feat: move contract data to one field

--- packages/react/src/hooks/useContractRead.ts
+++ packages/react/src/hooks/useContractRead.ts
@@ -1,83 +1,78 @@
-import { Contract, type Address, type JsonAbi, type Provider } from 'fuels';
-import type { FunctionNames, InputsForFunctionName } from 'src/types';
+import { Contract, type Address, type JsonAbi, type Provider } from "fuels";
+import type { FunctionNames, InputsForFunctionName } from "src/types";
 
-import { useNamedQuery } from '../core/useNamedQuery';
-import { QUERY_KEYS } from '../utils';
+import { useNamedQuery } from "../core/useNamedQuery";
+import { QUERY_KEYS } from "../utils";
+
+type ContractData<
+	TAbi extends JsonAbi
+> = {
+	address: Address;
+	abi: TAbi;
+	provider: Provider;
+};
 
 type ContractReadProps<
-  TAbi extends JsonAbi,
-  TFunctionName extends FunctionNames<TAbi>,
-> =
-  | {
-      address: Address;
-      abi: TAbi;
-      provider: Provider;
-      functionName: TFunctionName;
-      args: InputsForFunctionName<TAbi, TFunctionName>;
-      contract?: never;
-    }
-  | {
-      abi?: never;
-      address?: never;
-      provider?: never;
-      contract: Contract;
-      functionName: TFunctionName;
-      args: InputsForFunctionName<TAbi, TFunctionName>;
-    };
+	TAbi extends JsonAbi,
+	TFunctionName extends FunctionNames<TAbi>,
+> = {
+	contract: Contract | ContractData<TAbi>;
+	functionName: TFunctionName;
+	args: InputsForFunctionName<TAbi, TFunctionName>;
+};
 
 export const useContractRead = <
-  TAbi extends JsonAbi,
-  TFunctionName extends FunctionNames<TAbi>,
+	TAbi extends JsonAbi,
+	TFunctionName extends FunctionNames<TAbi>,
 >({
-  address,
-  abi,
-  functionName,
-  args,
-  contract: _contract,
-  provider,
+	functionName,
+	args,
+	contract: _contract,
 }: ContractReadProps<TAbi, TFunctionName>) => {
-  const isValid = !!_contract || (!!address && !!abi && !!provider);
-  const chainId = (_contract?.provider || provider)?.getChainId();
+  const isContractData = 'abi' in _contract && 'address' in _contract;
+  const { abi, address, provider } = (_contract as ContractData<TAbi>) ?? {};
+	const isValid = isContractData ? (!!abi && !!address && !!provider) : !!_contract;
+	const chainId = _contract?.provider?.getChainId();
 
-  return useNamedQuery('contract', {
-    queryKey: QUERY_KEYS.contract(
-      (_contract?.id || address || '')?.toString(),
-      chainId,
-      args?.toString(),
-    ),
-    queryFn: async () => {
-      if (!isValid) {
-        throw new Error(
-          'Contract or address, abi and provider are required to read the contract',
-        );
-      }
-      if (typeof chainId !== 'number') {
-        throw new Error('ChainId is required to read the contract');
-      }
-      const contract = _contract || new Contract(address, abi, provider);
+	return useNamedQuery("contract", {
+		queryKey: QUERY_KEYS.contract(
+			(isContractData ? address?.toString() : _contract?.id?.toString()),
+			chainId,
+			args?.toString(),
+		),
+		queryFn: async () => {
+			if (!isValid) {
+				throw new Error(
+					"Contract or address, abi and provider are required to read the contract",
+				);
+			}
+			if (typeof chainId !== "number") {
+				throw new Error("ChainId is required to read the contract");
+			}
+			const contract = (_contract as Contract) || new Contract(address, abi, provider); 
 
-      if (!contract?.functions?.[functionName]) {
-        throw new Error(`Function ${functionName || ''} not found on contract`);
-      }
+			if (!contract?.functions?.[functionName]) {
+				throw new Error(`Function ${functionName || ""} not found on contract`);
+			}
 
-      const wouldWriteToStorage =
-        contract.functions[functionName].isReadOnly?.() !== undefined
-          ? !contract.functions[functionName].isReadOnly()
-          : Object.values(contract.interface.functions)
-              .find((f) => f.name === functionName)
-              ?.attributes?.find((attr) => attr.name === 'storage')
-              ?.arguments?.includes('write');
+			const wouldWriteToStorage =
+				contract.functions[functionName].isReadOnly?.() !== undefined
+					? !contract.functions[functionName].isReadOnly()
+					: Object.values(contract.interface.functions)
+							.find((f) => f.name === functionName)
+							?.attributes?.find((attr) => attr.name === "storage")
+							?.arguments?.includes("write");
 
-      if (wouldWriteToStorage) {
-        throw new Error(
-          'Methods that write to storage should not be called with useContractRead',
-        );
-      }
+			if (wouldWriteToStorage) {
+				throw new Error(
+					"Methods that write to storage should not be called with useContractRead",
+				);
+			}
 
-      return args !== undefined
-        ? contract.functions[functionName](args)
-        : contract.functions[functionName]();
-    },
-    enabled: isValid,
-  });
+			return args !== undefined
+				? contract.functions[functionName](args)
+				: contract.functions[functionName]();
+		},
+		enabled: isValid,
+	});
 };
