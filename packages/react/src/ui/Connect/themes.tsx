930eb1f93a0f020d7c23b7c86216b0fba8339ed5
Pedro Pereira <pedro.2108@hotmail.com>
2024-06-20 15:22:30 -0300

    style: chango colors

--- packages/react/src/ui/Connect/themes.tsx
+++ packages/react/src/ui/Connect/themes.tsx
@@ -1,9 +1,8 @@
 const commonTheme = {
   /* Fonts */
   '--fuel-font-family':
-    '"SF Pro Rounded",ui-rounded,"Nunito",-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,"Apple Color Emoji",Arial,sans-serif,"Segoe UI Emoji","Segoe UI Symbol"',
+    '"Inter", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;',
   '--fuel-font-size': '16px',
-  '--fuel-color': 'hsla(0, 0%, 56.08%, 1)',
   '--fuel-letter-spacing': '-0.64px',
   /* Spacing */
   '--fuel-border-radius': '6px',
@@ -13,20 +12,22 @@ const commonTheme = {
 };
 
 const lightTheme = {
+  '--fuel-color': '#141414',
   '--fuel-color-bold': '#000000',
   '--fuel-dialog-background': 'white',
   '--fuel-overlay-background': 'rgba(71,88,107,0.24)',
-  '--fuel-connector-background': 'rgba(241,243,244,0.6)',
+  '--fuel-connector-background': 'rgb(250 250 250)',
   '--fuel-connector-hover': 'rgb(241 243 244)',
   '--fuel-border-color': 'hsl(210deg 9.52% 83.53%)',
   '--fuel-border-hover': 'hsla(0, 0%, 78.04%, 1)',
-  '--fuel-button-background': 'rgb(226 230 233)',
+  '--fuel-button-background': 'rgb(250 250 250)',
   '--fuel-button-background-hover': 'rgb(203 205 207)',
   '--fuel-loader-background':
     'linear-gradient(to right, hsl(0, 0%, 92%) 8%, hsl(0, 0%, 85%) 18%, hsl(0, 0%, 92%) 33%)',
 };
 
 const darkTheme = {
+  '--fuel-color': '#e4e7e7',
   '--fuel-color-bold': '#ffffff',
   '--fuel-dialog-background': 'hsla(0,0%,8.63%,1)',
   '--fuel-overlay-background': 'rgba(0,0,0,0.6)',
