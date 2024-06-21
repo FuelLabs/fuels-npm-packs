799e7bbad3cc547503c8f2fdac9599517ffd9b10
Pedro Pereira <pedro.2108@hotmail.com>
2024-06-21 10:06:37 -0300

    feat: add example dApp to run the connector

---
+++ examples/react-app/src/hooks/useWallet.ts
@@ -0,0 +1,89 @@
+import {
+  useAccount,
+  useBalance,
+  useConnectUI,
+  useFuel,
+  useWallet as useFuelWallet,
+  useIsConnected,
+} from '@fuels/react';
+import { useEffect, useState } from 'react';
+
+interface ICurrentConnector {
+  logo: string;
+  title: string;
+}
+
+const DEFAULT_CONNECTOR: ICurrentConnector = {
+  logo: '',
+  title: 'Wallet Demo',
+};
+
+export const useWallet = () => {
+  const { fuel } = useFuel();
+  const {
+    connect,
+    isConnecting,
+    isLoading: isLoadingConnectors,
+  } = useConnectUI();
+  const { isConnected, refetch: refetchConnected } = useIsConnected();
+  const {
+    account,
+    isLoading: isLoadingAccount,
+    isFetching: isFetchingAccount,
+  } = useAccount();
+
+  const address = account ?? '';
+
+  const { wallet, refetch: refetchWallet } = useFuelWallet(address);
+
+  const {
+    balance,
+    isLoading: isLoadingBalance,
+    isFetching: isFetchingBalance,
+  } = useBalance({ address });
+
+  const [currentConnector, setCurrentConnector] =
+    useState<ICurrentConnector>(DEFAULT_CONNECTOR);
+
+  useEffect(() => {
+    refetchConnected();
+  }, [refetchConnected]);
+
+  useEffect(() => {
+    if (!isConnected) {
+      setCurrentConnector(DEFAULT_CONNECTOR);
+      return;
+    }
+
+    const currentConnector = fuel.currentConnector();
+
+    const title = currentConnector?.name ?? DEFAULT_CONNECTOR.title;
+
+    const logo =
+      currentConnector && typeof currentConnector.metadata?.image === 'object'
+        ? currentConnector.metadata.image.dark ?? ''
+        : (currentConnector?.metadata?.image as string) ?? '';
+
+    setCurrentConnector({ logo, title });
+  }, [fuel.currentConnector, isConnected]);
+
+  const isLoading = [isLoadingAccount, isLoadingBalance].some(Boolean);
+
+  const isFetching = [isFetchingAccount, isFetchingBalance].some(Boolean);
+
+  return {
+    address,
+    account,
+    balance,
+    currentConnector,
+    isConnected,
+    isConnecting,
+    isLoading,
+    isFetching,
+    isLoadingConnectors,
+    wallet,
+    connect,
+    refetchConnected,
+    refetchWallet,
+  };
+};
---
+++ examples/react-app/src/main.tsx
@@ -0,0 +1,90 @@
+import {
+  BurnerWalletConnector,
+  FuelWalletConnector,
+  FuelWalletDevelopmentConnector,
+  FueletWalletConnector,
+} from '@fuels/connectors';
+import { WalletConnectConnector } from '@fuels/connectors/walletconnect';
+import { FuelProvider } from '@fuels/react';
+import * as Toast from '@radix-ui/react-toast';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { coinbaseWallet, walletConnect } from '@wagmi/connectors';
+import { http, createConfig, injected } from '@wagmi/core';
+import { mainnet, sepolia } from '@wagmi/core/chains';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App.tsx';
+import ScreenSizeIndicator from './components/screensize-indicator.tsx';
+import './index.css';
+// import { SolanaConnector } from "@fuels/connectors/solana";
+// import { defaultSolanaConfig } from "@web3modal/solana";
+
+const queryClient = new QueryClient();
+
+// ============================================================
+// WalletConnect Connector configurations
+// https://docs.walletconnect.com/web3modal/javascript/about
+// ============================================================
+const WC_PROJECT_ID = import.meta.env.VITE_APP_WC_PROJECT_ID;
+const METADATA = {
+  name: 'Wallet Demo',
+  description: 'Fuel Wallets Demo',
+  url: location.href,
+  icons: ['https://connectors.fuel.network/logo_white.png'],
+};
+const wagmiConfig = createConfig({
+  chains: [mainnet, sepolia],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+  },
+  connectors: [
+    injected({ shimDisconnect: false }),
+    walletConnect({
+      projectId: WC_PROJECT_ID,
+      metadata: METADATA,
+      showQrModal: false,
+    }),
+    coinbaseWallet({
+      appName: METADATA.name,
+      appLogoUrl: METADATA.icons[0],
+      darkMode: true,
+      reloadOnDisconnect: true,
+    }),
+  ],
+});
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <FuelProvider
+        theme="dark"
+        fuelConfig={{
+          connectors: [
+            new FuelWalletConnector(),
+            new FueletWalletConnector(),
+            new WalletConnectConnector({
+              wagmiConfig,
+              projectId: WC_PROJECT_ID,
+            }),
+            new FuelWalletDevelopmentConnector(),
+            new BurnerWalletConnector(),
+          ],
+        }}
+      >
+        <Toast.Provider>
+          <App />
+          <Toast.Viewport
+            id="toast-viewport"
+            className="fixed bottom-0 right-0 z-[100] m-0 flex w-[420px] max-w-[100vw] list-none flex-col gap-[10px] p-[var(--viewport-padding)] outline-none [--viewport-padding:_25px]"
+          />
+        </Toast.Provider>
+        <ScreenSizeIndicator />
+      </FuelProvider>
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
