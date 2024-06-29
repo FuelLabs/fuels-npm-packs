8823a0ed7009d8d544a6123cfa814f694424fb52
Hélcio Franco <helciofranco@me.com>
2024-06-28 20:09:20 -0400

    feat: add loading state when the connector is still not installed

--- packages/react/src/ui/Connect/components/Connector/Connector.tsx
+++ packages/react/src/ui/Connect/components/Connector/Connector.tsx
@@ -9,6 +9,8 @@ import {
   ConnectorImage,
   ConnectorTitle,
 } from './styles';
+import { useEffect, useState } from 'react';
+import { useConnectUI } from '../../../../providers/FuelUIProvider';
 
 type ConnectorProps = {
   theme?: string;
@@ -20,6 +22,29 @@ export function Connector({ className, connector, theme }: ConnectorProps) {
   const {
     install: { action, link, description },
   } = connector.metadata;
+
+  const {
+    dialog: { connect },
+  } = useConnectUI();
+  const [isLoading, setLoading] = useState(!connector.installed);
+
+  useEffect(() => {
+    const ping = async () => {
+      // If the connector have been detected from the SDK, we don't need to ping it again
+      if (connector.installed) return;
+
+      // Let's give a last chance to the connector to detect it
+      try {
+        await connector.ping();
+        connector.installed = true;
+        connect(connector);
+      } catch (_error) {
+        setLoading(false);
+      }
+    };
+
+    ping();
+  }, [connector, connect]);
 
   return (
     <div className={className}>
@@ -33,11 +58,15 @@ export function Connector({ className, connector, theme }: ConnectorProps) {
       </ConnectorImage>
       <ConnectorContent>
         <ConnectorTitle>{connector.name}</ConnectorTitle>
-        <ConnectorDescription>{description}</ConnectorDescription>
+        <ConnectorDescription>
+          {isLoading ? 'Loading...' : description}
+        </ConnectorDescription>
       </ConnectorContent>
-      <ConnectorButton href={link} target="_blank">
-        {action || 'Install'}
-      </ConnectorButton>
+      {!isLoading && (
+        <ConnectorButton href={link} target="_blank">
+          {action || 'Install'}
+        </ConnectorButton>
+      )}
     </div>
   );
 }
